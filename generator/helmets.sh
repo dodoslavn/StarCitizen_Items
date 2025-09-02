eabea22b0ef56e2ba253dbf193319e471431ceb3
Dodoslav Novak <dodoslavn@gmail.com>
2025-09-02 22:32:15 +0200

    init

---
+++ generator/helmets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+### HELMETS
+nodejs main.js
+
+rm ../public/helmets/index.html
+
+mkdir -p ../public/helmets/
+cat head.html >> ../public/helmets/index.html
+cat ../tmp/helmets.html >> ../public/helmets/index.html
+cat footer.html >> ../public/helmets/index.html
+
+for ID in $( cat ../tmp/helmets.txt )
+        do
+        echo " > Helmets - downloading ID: "$ID
+        wget "https://cstone.space/uifimages/"$ID".png" -O "../public/helmets/"$ID".png" 2>/dev/null
+        done
--- generator/main.sh
+++ generator/main.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
+
 mkdir -p ../tmp/
 
+./helmets.sh
 
-### HELMETS
-nodejs main.js
-
-rm ../public/helmets/index.html
-
-mkdir -p ../public/helmets/
-cat head.html >> ../public/helmets/index.html
-cat ../tmp/helmets.html >> ../public/helmets/index.html
-cat footer.html >> ../public/helmets/index.html
-
-for ID in $( cat ../tmp/helmets.txt )
-        do
-        echo " > Helmets - downloading ID: "$ID
-        wget "https://cstone.space/uifimages/"$ID".png" -O "../public/helmets/"$ID".png" 2>/dev/null
-        done
+  
