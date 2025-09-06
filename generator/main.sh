f36edd74040e2c49d3e960f9d79d4775b4057bae
Dodoslav Novak <dodoslavn@gmail.com>
2025-09-06 18:26:53 +0200

    fix

--- generator/main.sh
+++ generator/main.sh
@@ -6,7 +6,7 @@ mkdir -p ../tmp/
 rm ../public/index.html 2>/dev/null
 cat ../website/header.html >> ../public/index.html
 cat ../website/footer.html >> ../public/index.html
-cp ../website/default.css >> ../public/
+cp ../website/default.css ../public/
 
 ./helmets.sh
 
