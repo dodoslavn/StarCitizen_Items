0e4dc5750a870395d8dae72f4c43b83ceef1bab2
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2025-09-20 16:03:37 +0200

    Create arms.js

---
+++ generator/arms.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+
+const endpoints =
+{
+    helmets: "https://finder.cstone.space/GetArmors/Arms"
+}
+
+function save(name, data) {
+    fs.writeFile("../tmp/" + name, data, (err) => {
+        if (err) { console.error("Error saving file:", err); }
+    });
+}
+
+async function Helmets() {
+    console.log("########################");
+    console.log("### Arms");
+    ids = "";
+    html_light = "";
+    html_medium = "";
+    html_heavy = "";
+
+    try {
+        const response = await fetch(endpoints.helmets);
+        if (!response.ok) { throw new Error(`HTTP error! status: ${response.status}`); }
+
+        const helmetData = await response.json();
+        console.log(" > Arms - data downloaded");
+        for (const item of helmetData) {
+            console.log(" > Arms - name:", item.Name);
+            ids += " " + item.ItemId;
+
+            html = "<table><tr><td colspan='2'><h3>" + item.Name + "</h3></td></tr><tr>";
+			html += `<td><img src=/helmets/` + item.ItemId + `.png></td><td><ul>`;
+            for (const [key, value] of Object.entries(item)) 
+				{ 
+				value_new = String(value).replace(/\\n/g, "<br>");
+				if ( key != "Name" && key != "ItemId" && key != "ItemCodeName" )
+					{ html += `<li><strong>${key}:</strong> ${value_new}</li>`; }
+				}
+            html += "</ul></td>";
+            html += "</tr></table>\n";
+	    if ( item.Atype == "Heavy Armor" )
+		{ html_heavy += html; }
+	    else if ( item.Atype == "Medium Armor" )
+		{ html_medium += html; }
+	    else
+		 { html_light += html; }
+        }
+    }
+    catch (err) {
+        console.error(" > Issue downloading arms:", err.message);
+        process.exit(1);
+    }
+    save("arms.txt", ids);
+    save("arms-heavy.html", html_heavy);
+    save("arms-medium.html", html_medium);
+    save("arms-light.html", html_light);
+}
+
+Helmets();
