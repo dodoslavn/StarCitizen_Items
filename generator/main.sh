56bf78355f594a5ac463726111605d1ec9576108
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2025-11-03 11:06:51 +0100

    Add favicon.ico copy to public directory

--- generator/main.sh
+++ generator/main.sh
@@ -7,6 +7,7 @@ rm  ../public/index.html 2>/dev/null
 cat ../website/header.html >> ../public/index.html
 cat ../website/footer.html >> ../public/index.html
 cp  ../website/default.css ../public/
+cp  ../website/favicon.ico ../public/
 
 ./helmets.sh
 ./arms.sh
